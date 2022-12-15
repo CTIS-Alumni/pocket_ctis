325942caf921520c34919457f72680867a7864d9
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2022-12-15 20:02:19 +0300

    Add getStaticProps to UsersRoute

--- pocket-ctis/pages/user/index.js
+++ pocket-ctis/pages/user/index.js
@@ -3,7 +3,8 @@ import NavigationBar from '../../components/navbar/NavigationBar'
 import UserInfoSidebar from '../../components/UserInfoSidebar/UserInfoSidebar'
 import UsersInfoPanel from '../../components/UsersInfoPanel/UsersInfoPanel'
 
-const UsersRoute = () => {
+const UsersRoute = ({ users }) => {
+  console.log('users: ', users)
   return (
     <div style={{ height: '100vh' }}>
       <NavigationBar />
@@ -13,6 +14,18 @@ const UsersRoute = () => {
       </div>
     </div>
   )
+}
+
+//call API, using getStaticProps. This will be called before the page is served to the frontend
+//the result will be added to props object, which will be added to the corresponding component.
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:3000/api/users')
+  const users = await res.json()
+  return {
+    props: {
+      users,
+    },
+  }
 }
 
 export default UsersRoute
