588312a7e7679be4ff7635480cd2ebfc2f8a2747
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2022-12-17 01:40:19 +0300

    add highSchoolsList

---
+++ pocket-ctis/components/HighSchoolsList/HighSchoolsList.js
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Col,
+  Form,
+} from 'react-bootstrap'
+import SearchBar from '../SearchBar/SearchBar'
+import styles from './HighSchoolsList.module.css'
+
+const HighSchoolList = ({ highSchools }) => {
+  console.log(highSchools)
+  return (
+    <Container fluid style={{ backgroundColor: '#f9f9f9', paddingTop: 20 }}>
+      <h1>High Schools</h1>
+      <Row>
+        <Col
+          style={{
+            backgroundColor: 'white',
+            border: '1px solid #eee',
+            marginLeft: 10,
+            paddingTop: 10,
+            boxShadow: '5px 5px 10px 0px rgba(0,0,0,0.3)',
+          }}
+        >
+          <h5>Filters</h5>
+          <Form.Check type='checkbox' id={`is_internship`} label={`Turkey`} />
+        </Col>
+        <Col lg='10'>
+          <SearchBar />
+          <Row>
+            <Col>
+              <ListGroup
+                variant='flush'
+                style={{ boxShadow: '5px 5px 10px 0px rgba(0,0,0,0.3)' }}
+              >
+                {highSchools.map((highSchool) => (
+                  <ListGroupItem className={styles.listItem}>
+                    <Link
+                      href={`/user/companies/${highSchool.id}`}
+                      className='d-flex justify-content-between align-items-start'
+                    >
+                      <div>
+                        <h5>{highSchool.high_school_name}</h5>
+                        <span style={{ fontSize: 12, color: '#999' }}>
+                          {`${highSchool.country_name} - ${highSchool.city_name}`}
+                        </span>
+                      </div>
+                    </Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default HighSchoolList
--- pocket-ctis/pages/user/highSchools/index.js
+++ pocket-ctis/pages/user/highSchools/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import NavigationBar from '../../../components/navbar/NavigationBar'
+import UserInfoSidebar from '../../../components/UserInfoSidebar/UserInfoSidebar'
+import HighSchoolList from '../../../components/HighSchoolsList/HighSchoolsList'
 
-const HighSchoolList = ({ highSchools }) => {
-  console.log('high schools: ', highSchools)
-  return <div>HighSchoolList</div>
+const HighSchoolDashboard = ({ highschools }) => {
+  console.log('high schools: ', highschools)
+  return (
+    <div style={{ height: '100vh' }}>
+      <NavigationBar />
+      <div className='d-flex' style={{ height: '100%' }}>
+        <UserInfoSidebar />
+        <HighSchoolList highSchools={highschools} />
+      </div>
+    </div>
+  )
 }
 
 export async function getServerSideProps() {
   const res = await fetch('http://localhost:3000/api/highschools')
-  const highSchools = await res.json()
-  return { props: { highSchools } }
+  const { highschools } = await res.json()
+  return { props: { highschools } }
 }
 
-export default HighSchoolList
+export default HighSchoolDashboard
