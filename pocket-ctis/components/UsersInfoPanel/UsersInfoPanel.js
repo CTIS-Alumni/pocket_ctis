00eb98247c6f9036336f57ce75e68cfb37e4742f
pbbxvr <57944720+pbbxvr@users.noreply.github.com>
2022-12-17 15:34:26 +0300

    company-list-style
    styled the companies list page

--- pocket-ctis/components/UsersInfoPanel/UsersInfoPanel.js
+++ pocket-ctis/components/UsersInfoPanel/UsersInfoPanel.js
@@ -1,5 +1,6 @@
 import { Navbar, Container, Nav, Form, Button } from 'react-bootstrap'
 import AcademicUpdates from '../AcademicUpdates/AcademicUpdates'
+import SearchBar from '../SearchBar/SearchBar'
 import WorkUpdates from '../WorkUpdates/WorkUpdates'
 
 import styles from './UsersInfoPanel.module.scss'
@@ -12,17 +13,7 @@ const UsersInfoPanel = () => {
         <span className={styles.welcome_message}>Welcome back, </span>
         <span className={styles.welcome_name_surname}>Name Surname</span>
       </div>
-      <Form className={styles.panel_search}>
-        <Form.Control
-          type='search'
-          placeholder='Search'
-          className=''
-          aria-label='Search'
-        />
-        <Button className={styles.panel_search_button}>
-          Search
-        </Button>
-      </Form>
+      <SearchBar />
       <div>
         <WorkUpdates />
         <AcademicUpdates />
