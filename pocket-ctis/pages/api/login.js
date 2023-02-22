fbdf353b067d3e1d9643e20ca5e319df437f7245
silaypc <silaypcc@gmail.com>
2023-02-23 01:00:05 +0300

    login bcrypt

--- pocket-ctis/pages/api/login.js
+++ pocket-ctis/pages/api/login.js
@@ -1,52 +1,61 @@
 import {sign} from "../../helpers/jwtHelper";
 import {serialize} from "cookie";
 import {doquery} from "../../helpers/dbconnect";
+import {compare} from "bcrypt"
 
 
-export default async function(req,res){
+export default async function (req, res) {
     const {username, password} = JSON.parse(req.body);
-    try{
-        const query = "SELECT * FROM usercredential WHERE username = ? ";
-        const user = await doquery({query: query,values: [username]});
-        if(user.hasOwnProperty("error"))
-            res.status(500).json({error: user.error.message});
-        else {
-            try{
-                if(user.length > 0 && user[0].hashed === password){
-                    const access_token = await sign({
-                        user_id: user[0].user_id
-                    }, process.env.ACCESS_SECRET, 60*7);
-
-                    const refresh_token = await sign({
-                        user_id: user[0].user_id
-                    }, process.env.REFRESH_SECRET, 60*60*24*3);
-
-                    const serialCookie = serialize("PocketCTISJWT", access_token, {
-                        httpOnly: true,
-                        secure: process.env.NODE_ENV !== "development",
-                        sameSite: "strict",
-                        maxAge: 60*7,
-                        path: "/"
-                    });
-
-                    const refreshCookie = serialize("RefreshJWT", refresh_token, {
-                        httpOnly: true,
-                        secure: process.env.NODE_ENV !== "development",
-                        sameSite: "strict",
-                        maxAge: 60*60*24*3,
-                        path: "/"
-                    });
-
-                    res.setHeader("Set-Cookie", [serialCookie, refreshCookie]);
-                    res.status(200).json({message: "Authenticated Successfully"});
-                }else{
-                    res.status(401).json({message: "Invalid credentials"});
-                }
-            }catch(error){
+    try {
+            const query = "SELECT * FROM usercredential WHERE username = ? ";
+            const user = await doquery({query: query, values: [username]});
+            if (user.hasOwnProperty("error"))
                 res.status(500).json({error: user.error.message});
+            else {
+                try {
+                    if (user.length) {
+                        compare(password, user[0].hashed, async function (err, result){
+                            if(err)
+                                res.status(500).json({error: err.message});
+                            if(!result)
+                                res.status(401).json({message: "Invalid credentials!"});
+
+                            const access_token = await sign({
+                                user_id: user[0].user_id
+                            }, process.env.ACCESS_SECRET, 60 * 7);
+
+                            const refresh_token = await sign({
+                                user_id: user[0].user_id
+                            }, process.env.REFRESH_SECRET, 60 * 60 * 24 * 3);
+
+                            const serialCookie = serialize("PocketCTISJWT", access_token, {
+                                httpOnly: true,
+                                secure: process.env.NODE_ENV !== "development",
+                                sameSite: "strict",
+                                maxAge: 60 * 7,
+                                path: "/"
+                            });
+
+                            const refreshCookie = serialize("RefreshJWT", refresh_token, {
+                                httpOnly: true,
+                                secure: process.env.NODE_ENV !== "development",
+                                sameSite: "strict",
+                                maxAge: 60 * 60 * 24 * 3,
+                                path: "/"
+                            });
+
+                            res.setHeader("Set-Cookie", [serialCookie, refreshCookie]);
+                            res.status(200).json({message: "Authenticated Successfully"});
+                        });
+                    } else {
+                        res.status(401).json({message: "Invalid credentials"});
+                    }
+                } catch (error) {
+                    res.status(500).json({error: user.error.message});
+                }
             }
-        }
-        }catch(error) {
+
+    } catch (error) {
         res.status(500).json({error: error.message})
     }
 }
