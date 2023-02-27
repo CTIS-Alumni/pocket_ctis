7e863c442f86226bf7310585f9c643f0ec402602
silaypc <silaypcc@gmail.com>
2023-02-27 20:40:45 +0300

    profile id addition

--- pocket-ctis/pages/api/users/[user_id]/profile.js
+++ pocket-ctis/pages/api/users/[user_id]/profile.js
@@ -31,11 +31,11 @@ export default async function handler(req, res){
                 temp = "SELECT  id, email_address, visibility FROM useremail WHERE user_id = ? order by email_address asc";
                 queries.push({name: "emails", query: temp, values: [user_id]});
 
-                temp = "SELECT uhs.id, hs.high_school_name, uhs.visibility FROM userhighschool uhs JOIN highschool hs ON (uhs.high_school_id = hs.id) " +
+                temp = "SELECT uhs.id, hs.high_school_name, uhs.high_school_id, uhs.visibility FROM userhighschool uhs JOIN highschool hs ON (uhs.high_school_id = hs.id) " +
                     "WHERE uhs.user_id = ?";
                 queries.push({name: "high_school", query: temp, values: [user_id]});
 
-                temp = "SELECT ul.id, ul.city_id, ci.city_name, co.country_name, ul.visibility FROM userlocation ul JOIN city ci ON (ul.city_id = ci.id) " +
+                temp = "SELECT ul.id, ul.city_id, ci.city_name, ci.country_id, co.country_name, ul.visibility FROM userlocation ul JOIN city ci ON (ul.city_id = ci.id) " +
                     "JOIN country co ON (ci.country_id = co.id) WHERE ul.user_id = ?";
                 queries.push({name: "location", query: temp, values: [user_id]});
 
@@ -45,31 +45,31 @@ export default async function handler(req, res){
                 temp = "SELECT id, profile_picture, visibility FROM userprofilepicture WHERE user_id = ?";
                 queries.push({name: "profile_picture", query: temp, values: [user_id]});
 
-                temp = "SELECT ue.id, ex.exam_name, ue.grade, ue.visibility FROM userexam ue JOIN exam ex ON (ue.exam_id = ex.id) " +
+                temp = "SELECT ue.id, ex.exam_name, ue.exam_id, ue.grade, ue.visibility FROM userexam ue JOIN exam ex ON (ue.exam_id = ex.id) " +
                     "WHERE ue.user_id = ? order by ex.exam_name asc";
                 queries.push({name: "exams", query: temp, values: [user_id]});
 
-                temp = "SELECT us.id, sk.skill_name, us.skill_level, skt.type_name, us.skill_level, us.visibility " +
+                temp = "SELECT us.id, us.skill_id, sk.skill_name, sk.skill_type_id, us.skill_level, skt.type_name, us.skill_level, us.visibility " +
                     "FROM userskill us JOIN skill sk ON (us.skill_id = sk.id) " +
                     "JOIN skilltype skt ON (sk.skill_type_id = skt.id) " +
                     "WHERE us.user_id = ? order by sk.skill_type_id asc ";
                 queries.push({name: "skills", query: temp, values: [user_id]});
 
-                temp = "SELECT usm.id, sm.social_media_name, usm.link, usm.visibility " +
+                temp = "SELECT usm.id, sm.social_media_name, usm.social_media_id, usm.link, usm.visibility " +
                     "FROM usersocialmedia usm JOIN socialmedia sm ON (usm.social_media_id = sm.id) " +
                     "WHERE usm.user_id = ? order by sm.social_media_name asc ";
                 queries.push({name: "socials", query: temp, values: [user_id]});
 
-                temp = "SELECT uss.id, ss.society_name, uss.activity_status, uss.visibility " +
+                temp = "SELECT uss.id, uss.society_id, ss.society_name, uss.activity_status, uss.visibility " +
                     "FROM userstudentsociety uss JOIN studentsociety ss ON (uss.society_id = ss.id) " +
                     "WHERE uss.user_id = ? order by ss.society_name asc";
                 queries.push({name: "societies", query: temp, values: [user_id]});
 
-                temp = "SELECT uws.id, s.sector_name, uws.visibility FROM userwantsector uws JOIN sector s ON (uws.sector_id = s.id) " +
+                temp = "SELECT uws.id, uws.sector_id, s.sector_name, uws.visibility FROM userwantsector uws JOIN sector s ON (uws.sector_id = s.id) " +
                     "WHERE uws.user_id = ? order by s.sector_name asc";
                 queries.push({name: "wanted_sectors", query: temp, values: [user_id]});
 
-                temp = "SELECT w.id, c.company_name, wt.type_name, w.department, w.position, w.work_description, ci.city_name, co.country_name, w.start_date, w.end_date, w.visibility, w.is_current " +
+                temp = "SELECT w.id, w.company_id, c.company_name, wt.type_name, w.work_type_id, w.department, w.position, w.work_description, ci.city_name, co.country_name, w.start_date, w.end_date, w.visibility, w.is_current " +
                     "FROM workrecord w LEFT OUTER JOIN company c ON (w.company_id = c.id) " +
                     "JOIN worktype wt ON (w.work_type_id = wt.id) " +
                     "LEFT OUTER JOIN city ci ON (w.city_id = ci.id) " +
@@ -77,7 +77,7 @@ export default async function handler(req, res){
                     "WHERE w.user_id = ? order by w.start_date desc";
                 queries.push({name: "work_records", query: temp, values: [user_id]});
 
-                temp = "SELECT i.id, c.company_name, i.semester, i.department, i.start_date, i.end_date, i.rating, i.opinion, i.visibility " +
+                temp = "SELECT i.id, c.company_name, i.company_id, i.semester, i.department, i.start_date, i.end_date, i.rating, i.opinion, i.visibility " +
                     "FROM internshiprecord i JOIN company c ON (i.company_id = c.id) " +
                     "WHERE i.user_id = ? order by i.start_date desc";
                 queries.push({name: "internships", query: temp, values: [user_id]});
@@ -90,7 +90,7 @@ export default async function handler(req, res){
                     "WHERE er.user_id = ?";
                 queries.push({name: "erasmus", query: temp, values: [user_id]});
 
-                temp = "SELECT e.id, e.edu_inst_id, ei.inst_name, d.degree_name, e.name_of_program, e.start_date, e.end_date, e.visibility, e.is_current, " +
+                temp = "SELECT e.id, e.edu_inst_id, ei.inst_name, d.id as 'degree_type_id', d.degree_name, e.name_of_program, e.start_date, e.end_date, e.visibility, e.is_current, " +
                     "ci.city_name, co.country_name  " +
                     "FROM educationrecord e " +
                     "JOIN educationinstitute ei ON (e.edu_inst_id = ei.id)  " +
