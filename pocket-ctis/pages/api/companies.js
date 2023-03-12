cfc33b2b571bd3dd4a9c53961e8dbdc8723a3dea
silaypc <silaypcc@gmail.com>
2023-03-12 22:39:48 +0300

    count

--- pocket-ctis/pages/api/companies.js
+++ pocket-ctis/pages/api/companies.js
@@ -14,24 +14,32 @@ export default async function handler(req, res) {
     switch (method) {
         case "GET":
             let values = [];
+            let count_values = [];
             try {
                 let query;
+                let count_query;
 
-                if(req.query.count)
-                    query = "SELECT COUNT(c.id) as count ";
-                else
+
+                    count_query = "SELECT COUNT(c.id) as count ";
                     query = "SELECT c.id, c.company_name, c.sector_id, s.sector_name, c.is_internship ";
 
                 query += "FROM company c JOIN sector s on (c.sector_id = s.id) ";
+                count_query += "FROM company c JOIN sector s on (c.sector_id = s.id) ";
                 if (req.query.sector_id) {
                     query += "WHERE s.id = ? ";
+                    count_query += "WHERE s.id = ? ";
                     values.push(req.query.sector_id);
+                    count_values.push(req.query.sector_id);
                 }
                 if (req.query.name) {
                     if (query.indexOf("WHERE") !== -1)
                         query += "AND c.company_name LIKE CONCAT('%', ?, '%') "
                     else query += "WHERE c.company_name LIKE CONCAT('%', ?, '%') ";
+                    if(count_query.indexOf("WHERE") !== -1)
+                        count_query += "AND c.company_name LIKE CONCAT('%', ?, '%') ";
+                    else count_query += "WHERE c.company_name LIKE CONCAT('%', ?, '%') ";
                     values.push(req.query.name);
+                    count_values.push(req.query.name);
                 }
 
                 if(req.query.searchcol && req.query.search){
@@ -45,20 +53,33 @@ export default async function handler(req, res) {
                     if (query.indexOf("WHERE") !== -1)
                         query += "AND ( ";
                     else query += "WHERE ( ";
+                    if (count_query.indexOf("WHERE") !== -1)
+                        query += "AND ( ";
+                    else query += "WHERE ( ";
                     searchColumns.forEach(function(column){
                         query += column + " LIKE CONCAT('%', ?, '%') "
+                        count_query += column + " LIKE CONCAT('%', ?, '%') ";
                         if(column !== searchColumns[searchColumns.length - 1])
+                        {
                             query += "OR ";
+                            count_query += "OR";
+                        }
                         values.push(req.query.search);
+                        count_values.push(req.query.search);
                     });
                     query += ") ";
+                    count_query += ") ";
                 }
 
                 if (req.query.internship) {
                     if (query.indexOf("WHERE") !== -1)
                         query += "AND c.is_internship = ? "
                     else query += "WHERE c.is_internship = ? ";
+                    if (count_query.indexOf("WHERE") !== -1)
+                        count_query += "AND c.is_internship = ? "
+                    else count_query += "WHERE c.is_internship = ? ";
                     values.push(req.query.internship);
+                    count_values.push(req.query.internship);
                 }
 
                 if (req.query.column && columns.hasOwnProperty(req.query.column) && req.query.order && (req.query.order === "asc" ||req.query.order === "desc")) {
@@ -71,11 +92,12 @@ export default async function handler(req, res) {
                     values.push(req.query.offset);
                 }
 
-                const companies = await doquery({query: query, values: values});
-                if(companies.hasOwnProperty("error"))
-                    res.status(500).json({error: companies.error.message});
+                const data = await doquery({query: query, values: values});
+                const length = await doquery({query: count_query, values: count_values});
+                if(data.hasOwnProperty("error"))
+                    res.status(500).json({error: data.error.message});
                 else
-                    res.status(200).json({companies});
+                    res.status(200).json({data: data, length: length[0].count});
             }catch(error){
                 res.status(500).json({error: error.message, query: req.query});
             }
