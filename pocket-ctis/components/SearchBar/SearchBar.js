7e3d7e5b0e3c9703518e495f94e302444e4ab90f
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2023-03-19 21:54:27 +0300

    Add pagination fix

--- pocket-ctis/components/SearchBar/SearchBar.js
+++ pocket-ctis/components/SearchBar/SearchBar.js
@@ -1,7 +1,14 @@
 import { Formik, Field, Form } from 'formik'
+import { XCircleFill } from 'react-bootstrap-icons'
 import styles from './SearchBar.module.scss'
 
 const SearchBar = ({ onSubmit, searchValue = '' }) => {
+  const clearSearch = (val) => {
+    if (val != '') {
+      onSubmit({ searchValue: '' })
+    }
+  }
+
   return (
     <div className={styles.search}>
       <Formik
@@ -9,18 +16,38 @@ const SearchBar = ({ onSubmit, searchValue = '' }) => {
         enableReinitialize
         onSubmit={onSubmit}
       >
-        <Form className={styles.search_form}>
-          <label className={styles.search_bar_label} htmlFor='searchValue'>Search</label>
-          <Field
-            className={styles.search_bar}
-            id='searchValue'
-            name='searchValue'
-            placeholder='Please enter...'
-          />
-          <button type='submit' className={styles.search_button}>
-            Search
-          </button>
-        </Form>
+        {(props) => (
+          <Form className={styles.search_form}>
+            <>
+              <label className={styles.search_bar_label} htmlFor='searchValue'>
+                Search
+              </label>
+              <div style={{ position: 'relative', width: '100%' }}>
+                <Field
+                  className={styles.search_bar}
+                  id='searchValue'
+                  name='searchValue'
+                  placeholder='Please enter...'
+                />
+                <XCircleFill
+                  style={{
+                    position: 'absolute',
+                    right: '10px',
+                    top: '1.3em',
+                    fontSize: '10px',
+                  }}
+                  onClick={() => {
+                    clearSearch(props.values.searchValue)
+                    props.setFieldValue('searchValue', '')
+                  }}
+                />
+              </div>
+              <button type='submit' className={styles.search_button}>
+                Search
+              </button>
+            </>
+          </Form>
+        )}
       </Formik>
     </div>
   )
