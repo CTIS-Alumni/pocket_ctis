3f1e116cd7899617c268da75ec070a3c231c4586
silaypc <silaypcc@gmail.com>
2023-05-13 00:25:01 +0300

    fix

--- pocket-ctis/pages/api/basic_info.js
+++ pocket-ctis/pages/api/basic_info.js
@@ -6,11 +6,11 @@ export default async function handler(req, res) {
     const payload = await checkUserType(session, req.query);
     if (session && payload) {
         try{
-            const data_query = "SELECT u.id, u.first_name, u.last_name, u.is_active, upp.profile_picture, GROUP_CONCAT(act.type_name) as 'user_types' " +
+            const data_query = "SELECT u.id, u.first_name, u.last_name, u.is_active, upp.profile_picture, GROUP_CONCAT(DISTINCT act.type_name) as 'user_types' " +
                 "FROM users u LEFT OUTER JOIN usercredential uc ON (u.id = uc.user_id) JOIN useraccounttype uat ON (uat.user_id = u.id) " +
                 "JOIN accounttype act ON (act.id = uat.type_id) " +
                 "LEFT OUTER JOIN userprofilepicture upp ON (upp.user_id = u.id) " +
-                "WHERE u.id = ? GROUP BY u.id "
+                "WHERE u.id = ?"
 
             const data = await doquery({query: data_query, values: [payload.user_id]});
             res.status(200).json({data});
