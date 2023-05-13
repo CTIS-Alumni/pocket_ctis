4425b515d026c9754aab23f1a3081ca05dcff7dc
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2023-05-13 04:08:42 +0300

    CODE-MAX: Add user reg form

--- pocket-ctis/pages/admin/users/index.js
+++ pocket-ctis/pages/admin/users/index.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 import { _getFetcher } from '../../../helpers/fetchHelpers'
 import { craftUrl } from '../../../helpers/urlHelper'
-import { Container, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Container, ListGroup, ListGroupItem, Tabs, Tab } from 'react-bootstrap'
 import Link from 'next/link'
 import { getProfilePicturePath } from '../../../helpers/formatHelpers'
 import AdminPageContainer from '../../../components/AdminPanelComponents/AdminPageContainer/AdminPageContainer'
+import styles from '../../../styles/adminUsersList.module.css'
+import { ArrowLeft } from 'react-bootstrap-icons'
+import CreateUserForm from '../../../components/AdminPanelComponents/CreateUserForm/CreateUserForm'
 
 const AdminUsersList = () => {
+  const [activeKey, setActiveKey] = useState('create')
   const [users, setUsers] = useState([])
 
   useEffect(() => {
@@ -18,40 +22,56 @@ const AdminUsersList = () => {
   return (
     <AdminPageContainer>
       <Container>
-        <ListGroup variant='flush'>
-          {users.map((user, i) => {
-            return (
-              <ListGroupItem style={{ width: '100%' }} key={i}>
-                <Link href={`/admin/users/${user.id}`}>
-                  <div style={{ display: 'flex' }}>
-                    <img
-                      width={80}
-                      height={80}
-                      style={{ objectFit: 'contain', borderRadius: '50%' }}
-                      src={getProfilePicturePath(
-                        user.pic_visibility,
-                        user.profile_picture
-                      )}
-                    />
-                    <div
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        flexFlow: 'column',
-                        paddingLeft: '0.8em',
-                      }}
-                    >
-                      <div>
-                        {user.id} - {user.first_name} {user.last_name}
+        <Tabs style={{ display: 'none' }} activeKey={activeKey}>
+          <Tab eventKey='display' title='display'>
+            <div className='d-flex justify-content-between align-items-center'>
+              <h4>Users</h4>
+              <button
+                className={styles.createUserButton}
+                onClick={() => setActiveKey('create')}
+              >
+                Create User
+              </button>
+            </div>
+            <ListGroup variant='flush'>
+              {users.map((user, i) => {
+                return (
+                  <ListGroupItem style={{ width: '100%' }} key={i}>
+                    <Link href={`/admin/users/${user.id}`}>
+                      <div style={{ display: 'flex' }}>
+                        <img
+                          width={80}
+                          height={80}
+                          style={{ objectFit: 'contain', borderRadius: '50%' }}
+                          src={getProfilePicturePath(
+                            user.pic_visibility,
+                            user.profile_picture
+                          )}
+                        />
+                        <div
+                          style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            flexFlow: 'column',
+                            paddingLeft: '0.8em',
+                          }}
+                        >
+                          <div>
+                            {user.id} - {user.first_name} {user.last_name}
+                          </div>
+                          <Container>{user.user_types}</Container>
+                        </div>
                       </div>
-                      <Container>{user.user_types}</Container>
-                    </div>
-                  </div>
-                </Link>
-              </ListGroupItem>
-            )
-          })}
-        </ListGroup>
+                    </Link>
+                  </ListGroupItem>
+                )
+              })}
+            </ListGroup>
+          </Tab>
+          <Tab eventKey='create' title='create'>
+            <CreateUserForm goBack={() => setActiveKey('display')} />
+          </Tab>
+        </Tabs>
       </Container>
     </AdminPageContainer>
   )
