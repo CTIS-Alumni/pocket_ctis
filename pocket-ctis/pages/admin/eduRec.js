8a3b08e9bfa976279b9e5a81e1f226c4f83c29ca
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2023-05-19 01:43:39 +0300

    work rec dash

---
+++ pocket-ctis/pages/admin/eduRec.js
@@ -0,0 +1,7 @@
+import AdminPageContainer from '../../components/AdminPanelComponents/AdminPageContainer/AdminPageContainer'
+
+const EduRec = () => {
+  return <AdminPageContainer>eduRec</AdminPageContainer>
+}
+
+export default EduRec
