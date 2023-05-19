9deb75947d42aa6098f9c7ff689a2ba7517fb66d
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2023-05-19 21:48:17 +0300

    fix

--- pocket-ctis/components/DataTable/DataTable.js
+++ pocket-ctis/components/DataTable/DataTable.js
@@ -81,24 +81,21 @@ const DataTable = ({
     setToDelete(d)
   }
 
-  const deletePopover = useMemo(
-    () => (
-      <Popover title='Delete?'>
-        <div className={styles.popoverBody}>
-          Are you sure you want to delete this?
-        </div>
-        <button
-          className={styles.popoverDeleteBtn}
-          onClick={() => {
-            deleteHandler(toDelete)
-            document.body.click()
-          }}
-        >
-          Confirm
-        </button>
-      </Popover>
-    ),
-    []
+  const deletePopover = (
+    <Popover title='Delete?'>
+      <div className={styles.popoverBody}>
+        Are you sure you want to delete this?
+      </div>
+      <button
+        className={styles.popoverDeleteBtn}
+        onClick={() => {
+          deleteHandler(toDelete)
+          document.body.click()
+        }}
+      >
+        Confirm
+      </button>
+    </Popover>
   )
 
   return (
