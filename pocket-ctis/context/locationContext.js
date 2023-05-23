03e4f81e32fc7b9c0cfa479d6cd67e129154aaa7
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2023-05-23 03:46:18 +0300

    fix

--- pocket-ctis/context/locationContext.js
+++ pocket-ctis/context/locationContext.js
@@ -1,6 +1,7 @@
-import { createContext, useState, useEffect } from 'react'
+import { createContext, useState, useEffect, useContext } from 'react'
 import { _getFetcher } from '../helpers/fetchHelpers'
 import { craftUrl } from '../helpers/urlHelper'
+import { User_data } from './userContext'
 
 export const Location_data = createContext({
   locationData: null,
@@ -9,24 +10,27 @@ export const Location_data = createContext({
 
 function LocationContext({ children }) {
   const [locationData, setLocationData] = useState()
+  const {userData} = useContext(User_data)
 
   useEffect(() => {
-    let data = {}
-    _getFetcher({ cities: craftUrl(['cities']) }).then(({ cities }) => {
-      cities.data.forEach((city) => {
-        if (`${city.country_id}-${city.country_name}` in data) {
-          data[`${city.country_id}-${city.country_name}`].push(
-            `${city.id}-${city.city_name}`
-          )
-        } else {
-          data[`${city.country_id}-${city.country_name}`] = [
-            `${city.id}-${city.city_name}`,
-          ]
-        }
+    if (userData != null){
+      let data = {}
+      _getFetcher({ cities: craftUrl(['cities']) }).then(({ cities }) => {
+        cities.data.forEach((city) => {
+          if (`${city.country_id}-${city.country_name}` in data) {
+            data[`${city.country_id}-${city.country_name}`].push(
+              `${city.id}-${city.city_name}`
+            )
+          } else {
+            data[`${city.country_id}-${city.country_name}`] = [
+              `${city.id}-${city.city_name}`,
+            ]
+          }
+        })
       })
-    })
-    setLocationData(data)
-  }, [])
+      setLocationData(data)
+    }
+  }, [userData])
 
   return (
     <Location_data.Provider value={{ locationData, setLocationData }}>
